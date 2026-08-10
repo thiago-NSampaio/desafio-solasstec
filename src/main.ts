5e6ebdf3795953183f0848509a83dd183b0ca852
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2026-08-09 21:44:44 -0400

    chore(server): enable CORS and update default server port

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 3300;
 
   await app.listen(port);
 }
